e56adc4140ce9c9d2824de40bb74b1740fa03850
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2023-05-30 15:43:16 +0300

    refactoring HW 02:05

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import { Section } from './AppStyled';
 import { PhoneBookForm } from './PhoneBookForm';
 import { Filter } from './Filter';
 import { PhoneBookList } from './PhoneBookList/PhoneBookList';
-import { nanoid } from 'nanoid';
+
 const INITIAL_STATE = {
   contacts: [
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
@@ -14,53 +14,32 @@ const INITIAL_STATE = {
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
   ],
   filter: '',
-  name: '',
-  number: '',
 };
+
 export class App extends Component {
   state = { ...INITIAL_STATE };
-  changeBook = evt => {
-    evt.preventDefault();
-    const newContact = {
-      name: this.state.name,
-      id: nanoid(),
-      number: this.state.number,
-    };
+
+  changeContacts = newContact => {
     this.setState(prevState => {
-      return {
-        contacts: [...prevState.contacts, newContact],
-      };
+      return { contacts: [...prevState.contacts, newContact] };
     });
-    this.reset();
   };
-  handleChangeName = ({ target }) => {
-    this.setState({ [target.name]: target.value });
-  };
-  handleChangeTel = ({ target }) => {
-    this.setState({ [target.name]: target.value });
-  };
+
   handleChangeSearch = ({ target }) => {
     this.setState({ filter: target.value.toLowerCase().trim() });
   };
+
   arrayWithSearch = () => {
     const searchQuery = this.state.filter;
     const filtered = this.state.contacts.filter(el => el.name.toLowerCase().includes(searchQuery));
     return filtered;
   };
-  reset = () => {
-    this.setState({ name: '', number: '' });
-  };
+
   render() {
     return (
       <Section>
         <MainTitle>Phonebook</MainTitle>
-        <PhoneBookForm
-          changeBook={this.changeBook}
-          nameValue={this.state.name}
-          handleChangeName={this.handleChangeName}
-          telValue={this.state.number}
-          handleChangeTel={this.handleChangeTel}
-        ></PhoneBookForm>
+        <PhoneBookForm changeContacts={this.changeContacts} />
         <HTwo>Contacts</HTwo>
         <Filter handleChangeSearch={this.handleChangeSearch} />
         <PhoneBookList array={this.arrayWithSearch()} />
--- src/components/PhoneBookForm/PhoneBookForm.jsx
+++ src/components/PhoneBookForm/PhoneBookForm.jsx
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { BookForm, NameInput, AddBtn, Paragraph, Div, TelInput } from './PhoneBookFormStyled';
+import { nanoid } from 'nanoid';
 export class PhoneBookForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  changeBook = (evt, changeContacts) => {
+    evt.preventDefault();
+    const newContact = {
+      name: evt.target.name.value,
+      id: nanoid(),
+      number: evt.target.number.value,
+    };
+    changeContacts(newContact);
+    this.reset();
+  };
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+  handleChangeNameTel = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  };
+
   render() {
-    const { nameValue, handleChangeName, telValue, handleChangeTel, changeBook } = this.props;
+    const { changeContacts } = this.props;
+
+    const { name: nameValue, number: telValue } = this.state;
     return (
-      <BookForm onSubmit={evt => changeBook(evt)}>
+      <BookForm onSubmit={evt => this.changeBook(evt, changeContacts)}>
         <Div>
           <Paragraph>Name</Paragraph>
           <NameInput
@@ -15,7 +40,7 @@ export class PhoneBookForm extends Component {
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
             value={nameValue}
-            onChange={evt => handleChangeName(evt)}
+            onChange={evt => this.handleChangeNameTel(evt)}
           />
           <Paragraph>Number</Paragraph>
           <TelInput
@@ -25,7 +50,7 @@ export class PhoneBookForm extends Component {
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
             value={telValue}
-            onChange={evt => handleChangeTel(evt)}
+            onChange={evt => this.handleChangeNameTel(evt)}
           ></TelInput>
           <AddBtn type="submit">Add contact</AddBtn>
         </Div>
@@ -34,9 +59,12 @@ export class PhoneBookForm extends Component {
   }
 }
 PhoneBookForm.propTypes = {
-  changeBook: PropTypes.func.isRequired,
-  nameValue: PropTypes.string.isRequired,
-  handleChangeName: PropTypes.func.isRequired,
-  telValue: PropTypes.string.isRequired,
-  handleChangeTel: PropTypes.func.isRequired,
+  changeContacts: PropTypes.func.isRequired,
 };
+// PhoneBookForm.propTypes = {
+//   changeBook: PropTypes.func.isRequired,
+//   nameValue: PropTypes.string.isRequired,
+//   handleChangeName: PropTypes.func.isRequired,
+//   telValue: PropTypes.string.isRequired,
+//   handleChangeTel: PropTypes.func.isRequired,
+// };
